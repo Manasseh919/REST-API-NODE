2786efc6174d0ab8eb94655e47ece539c235b34f
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-04-14 18:45:01 +0000

    logging,error handling

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
-const productsRoutes = require("./api/routes/products");
-const orderRoutes = require('./api/routes/order')
-
+const morgan = require("morgan");
 const app = express();
 
+/* Routes */
+const productsRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/order");
+
+app.use(morgan("dev"));
+
+/* Routes for handling of request */
 app.use("/products", productsRoutes);
-app.use("/order", orderRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
